953e7e12b43a3c965a380fc6d3b99a6c518633ad
HoangNhung <hoang.thi.nhung@framgia.com>
2015-12-30 11:31:56 +0700

    devise User model

---
+++ config/initializers/sabisu_rails.rb
@@ -0,0 +1,10 @@
+SabisuRails.setup do |config|
+  config.base_api_uri = "api.marketplaceapi.dev"
+
+  config.api_headers = {"Accept" => "application/vnd.marketplace.v1"}
+
+  config.resources = [:users]
+
+  config.default_resource = :users
+
+end
